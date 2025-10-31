4bb0af421e4fec45e72a7cd7efb95cdb57049b81
KEIHART.online <john@keihart.online>
2025-10-31 14:05:14 +0100

    ADD: added product type enum

---
+++ src/Enums/ProductTypeEnum.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace KeihartOnline\JouwHoesjeApi\Enums;
+
+enum ProductTypeEnum: string
+{
+    case COVER = 'cover';
+    case PRODUCT = 'product';
+    case CUSTOM_DESIGN = 'custom-design';
+}
