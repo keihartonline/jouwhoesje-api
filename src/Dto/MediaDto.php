668fbd9a2f4fa9632af23d5552d7c9abba8b81f7
KEIHART.online <john@keihart.online>
2025-11-13 10:39:07 +0100

    CHG: update signature

--- src/Dto/MediaDto.php
+++ src/Dto/MediaDto.php
@@ -8,17 +8,17 @@ final readonly class MediaDto
      * @param  array<string, string>  $conversions  Key is the name of the conversion, value is the URL
      */
     public function __construct(
-        public ?string $caption,
-        public ?string $title,
         public array $conversions,
+        public ?string $caption = null,
+        public ?string $title = null,
     ) {}
 
     public static function fromArray(array $data): self
     {
         return new self(
+            conversions: $data['conversions'],
             caption: $data['caption'] ?? null,
             title: ($data['title'] ?? $data['caption']) ?? null,
-            conversions: $data['conversions'],
         );
     }
 }
