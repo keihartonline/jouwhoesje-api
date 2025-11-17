b478f02f61e46bdd75c30583399b9b1af936f397
KEIHART.online <john@keihart.online>
2025-11-17 09:55:13 +0100

    CHG: handle is not required

--- src/Dto/MediaDto.php
+++ src/Dto/MediaDto.php
@@ -8,8 +8,8 @@ final readonly class MediaDto
      * @param  array<string, string>  $conversions  Key is the name of the conversion, value is the URL
      */
     public function __construct(
-        public string|int|null $handle,
         public array $conversions,
+        public string|int|null $handle = null,
         public ?string $caption = null,
         public ?string $title = null,
     ) {}
@@ -17,8 +17,8 @@ final readonly class MediaDto
     public static function fromArray(array $data): self
     {
         return new self(
-            handle: $data['handle'] ?? null,
             conversions: $data['conversions'],
+            handle: $data['handle'] ?? null,
             caption: $data['caption'] ?? null,
             title: ($data['title'] ?? $data['caption']) ?? null,
         );
