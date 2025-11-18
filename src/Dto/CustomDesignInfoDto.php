4b12ab7cde8ce6320c7d181829dbd0dfb32faf32
KEIHART.online <john@keihart.online>
2025-11-18 14:57:54 +0100

    CHG: simplified custom design info

--- src/Dto/CustomDesignInfoDto.php
+++ src/Dto/CustomDesignInfoDto.php
@@ -5,30 +5,22 @@ namespace KeihartOnline\JouwHoesjeApi\Dto;
 final readonly class CustomDesignInfoDto
 {
     /**
-     * @param  CustomDesignGroupDto[]  $groups
+     * @param  CustomDesignOptionDto[]  $options
      * @param  MediaDto[]  $media
      */
     public function __construct(
-        public string $sku,
         public array $prices,
-        public array $groupNames,
-        public int $groupsCount,
-        public int $optionsCount,
-        public array $groups,
+        public array $options,
         public array $media,
     ) {}
 
     public static function fromArray(array $data): self
     {
         return new self(
-            sku: $data['sku'],
             prices: $data['prices'],
-            groupNames: $data['group_names'],
-            groupsCount: $data['groups_count'] ?? 0,
-            optionsCount: $data['options_count'] ?? 0,
-            groups: array_map(
-                fn (array $row) => CustomDesignGroupDto::fromArray($row),
-                $data['groups']
+            options: array_map(
+                fn (array $row) => CustomDesignOptionDto::fromArray($row),
+                $data['options']
             ),
             media: array_map(
                 fn (array $row) => MediaDto::fromArray($row),
--- src/Dto/CustomDesignOptionDto.php
+++ src/Dto/CustomDesignOptionDto.php
@@ -4,21 +4,27 @@ namespace KeihartOnline\JouwHoesjeApi\Dto;
 
 final readonly class CustomDesignOptionDto
 {
+    /**
+     * @param  CustomDesignOptionDto[]  $options
+     */
     public function __construct(
-        public string $sku,
-        public int $price,
+        public string $skus,
+        public int $prices,
         public string $name,
-        public bool $hasVariants,
+        public array $options = [],
         public ?MediaDto $firstMedia = null,
     ) {}
 
     public static function fromArray(array $data): self
     {
         return new self(
-            sku: $data['sku'],
-            price: $data['price'],
+            skus: $data['skus'],
+            prices: $data['prices'],
             name: $data['name'],
-            hasVariants: $data['has_variants'] ?? false,
+            options: array_map(
+                fn (array $row) => CustomDesignOptionDto::fromArray($row),
+                $data['options'] ?? []
+            ),
             firstMedia: ! blank($data['first_media'])
                 ? MediaDto::fromArray($data['first_media'])
                 : null,
