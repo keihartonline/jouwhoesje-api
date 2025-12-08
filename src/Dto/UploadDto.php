2ec3e3a11403d97e2b15984b3eb54c518cc231b9
KEIHART.online <john@keihart.online>
2025-12-08 15:54:41 +0100

    CHG: extend upload dto with hasTransparency

--- src/Dto/UploadDto.php
+++ src/Dto/UploadDto.php
@@ -6,6 +6,7 @@ final readonly class UploadDto
 {
     public function __construct(
         public string $name,
+        public bool $hasTransparency,
         public string $path,
         public MediaDto $media,
     ) {}
@@ -14,6 +15,7 @@ final readonly class UploadDto
     {
         return new self(
             name: $data['name'],
+            hasTransparency: $data['has_transparency'],
             path: $data['path'],
             media: MediaDto::fromArray($data['media']),
         );
