3d1944b00ae137777743af97d79c917d081b33fc
KEIHART.online <john@keihart.online>
2025-12-17 11:07:30 +0100

    CHG: add scale to enum

--- src/Enums/FitTypeContainSizeEnum.php
+++ src/Enums/FitTypeContainSizeEnum.php
@@ -7,4 +7,13 @@ enum FitTypeContainSizeEnum: string
     case SMALL = 'small';
     case LARGER = 'larger';
     case LARGEST = 'largest';
+
+    public function scale(): float
+    {
+        return match ($this) {
+            self::SMALL => 0.6,
+            self::LARGER => 0.8,
+            self::LARGEST => 1,
+        };
+    }
 }
