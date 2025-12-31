e65d917de6efecc3539d8cbe972b1b348c71dc61
KEIHART.online <john@keihart.online>
2025-12-31 17:00:36 +0100

    CHG: wrong key

--- src/Dto/CartDto.php
+++ src/Dto/CartDto.php
@@ -35,8 +35,8 @@ final readonly class CartDto
             totalPriceNet: (int) ($data['total_price_net'] ?? 0),
             totalGiftPackagingGross: (int) ($data['total_gift_packaging_gross'] ?? 0),
             totalGiftPackagingNet: (int) ($data['total_gift_packaging_net'] ?? 0),
-            totalProductsGross: (int) ($data['total_price_gross'] ?? 0),
-            totalProductsNet: (int) ($data['total_price_net'] ?? 0),
+            totalProductsGross: (int) ($data['total_products_gross'] ?? 0),
+            totalProductsNet: (int) ($data['total_products_net'] ?? 0),
             totalVat: (int) ($data['total_vat'] ?? 0),
             minimumOrderValue: (int) ($data['minimum_order_value'] ?? 0),
             items: array_map(
