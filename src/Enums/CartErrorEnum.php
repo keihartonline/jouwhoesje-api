257866ffaae7ab154e2e72119e5a576be44ff350
KEIHART.online <john@keihart.online>
2026-01-23 10:41:48 +0100

    CHG: add missing-company-name to cart errors

--- src/Enums/CartErrorEnum.php
+++ src/Enums/CartErrorEnum.php
@@ -5,6 +5,7 @@ namespace KeihartOnline\JouwHoesjeApi\Enums;
 enum CartErrorEnum: string
 {
     case INCORRECT_EMAIL = 'incorrect-email';
+    case MISSING_COMPANY_NAME = 'missing-company-name';
     case MISSING_SHIPPING_ADDRESS = 'missing-shipping-address';
     case INCORRECT_INVOICE_ADDRESS = 'incorrect-invoice-address';
     case INCORRECT_SHIPPING_ADDRESS = 'incorrect-shipping-address';
