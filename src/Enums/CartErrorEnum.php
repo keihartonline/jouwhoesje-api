dd46ee5adf786633c1ba55288f7fcbd15cced431
KEIHART.online <john@keihart.online>
2026-01-26 09:20:13 +0100

    CHG: remove VAT_NUMBER_INCORRECT enum

--- src/Enums/CartErrorEnum.php
+++ src/Enums/CartErrorEnum.php
@@ -6,7 +6,6 @@ enum CartErrorEnum: string
 {
     case INCORRECT_EMAIL = 'incorrect-email';
     case MISSING_COMPANY_NAME = 'missing-company-name';
-    case VAT_NUMBER_INCORRECT = 'vat-number-incorrect';
     case MISSING_SHIPPING_ADDRESS = 'missing-shipping-address';
     case INCORRECT_INVOICE_ADDRESS = 'incorrect-invoice-address';
     case INCORRECT_SHIPPING_ADDRESS = 'incorrect-shipping-address';
