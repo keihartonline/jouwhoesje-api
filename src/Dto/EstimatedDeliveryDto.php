975eaf6da14fc9ac8c1a7978489650394b6354fb
KEIHART.online <john@keihart.online>
2026-01-27 18:58:54 +0100

    CHG: extent deliery dto

--- src/Dto/EstimatedDeliveryDto.php
+++ src/Dto/EstimatedDeliveryDto.php
@@ -6,7 +6,10 @@ final readonly class EstimatedDeliveryDto
 {
     public function __construct(
         public int $shippingRuleId,
-        public string $cutoff,
+        public string $cutoffDay,
+        public string $cutoffTime,
+        public bool $cutoffIsToday,
+        public bool $cutoffIsTomorrow,
         public string $firstDay,
         public string $lastDay,
         public bool $hasRange,
@@ -16,7 +19,10 @@ final readonly class EstimatedDeliveryDto
     {
         return new self(
             shippingRuleId: $data['shipping_rule_id'],
-            cutoff: $data['cutoff'],
+            cutoffDay: $data['cutoff_day'],
+            cutoffTime: $data['cutoff_time'],
+            cutoffIsToday: $data['cutoff_is_today'],
+            cutoffIsTomorrow: $data['cutoff_is_tomorrow'],
             firstDay: $data['first_day'],
             lastDay: $data['last_day'],
             hasRange: $data['has_range'],
