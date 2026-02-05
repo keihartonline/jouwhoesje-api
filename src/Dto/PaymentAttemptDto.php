8d04b40bc1097e12257b8c2f6d5753d2d8a01d78
KEIHART.online <john@keihart.online>
2026-02-05 16:07:27 +0100

    CHG: add handy attributes to dto

--- src/Dto/PaymentAttemptDto.php
+++ src/Dto/PaymentAttemptDto.php
@@ -12,6 +12,9 @@ final readonly class PaymentAttemptDto
         public ?string $redirectUrl,
         public bool $canRetry,
         public int $retryInSeconds,
+        public bool $hasOrder,
+        public ?string $orderNumber,
+        public ?string $email,
     ) {}
 
     public static function fromArray(array $data): self
@@ -22,6 +25,9 @@ final readonly class PaymentAttemptDto
             redirectUrl: $data['redirect_url'],
             canRetry: $data['can_retry'],
             retryInSeconds: $data['retry_in_seconds'],
+            hasOrder: $data['has_order'],
+            orderNumber: $data['order_number'] ?? null,
+            email: $data['email'] ?? null,
         );
     }
 }
