d13c192c4e56ccf24e2c3e2b2f2b92efa8698488
KEIHART.online <john@keihart.online>
2026-02-13 10:21:28 +0100

    CHG: extend order dto

--- src/Dto/OrderDto.php
+++ src/Dto/OrderDto.php
@@ -10,6 +10,16 @@ final readonly class OrderDto
     public function __construct(
         public string $orderNumber,
         public OrderStatusEnum $status,
+        public string $email,
+        public int $vatRate,
+        public bool $isCompany,
+        public ?string $companyName,
+        public ?string $vatNumber,
+        public int $totalPriceGross,
+        public int $shippingCostsGross,
+        public AddressDto $invoiceAddress,
+        public ?AddressDto $shippingAddress,
+        public ShippingRuleDto $shippingRule,
         public Carbon $createdAt,
         public Carbon $updatedAt,
     ) {}
@@ -19,6 +29,18 @@ final readonly class OrderDto
         return new self(
             orderNumber: $data['order_number'],
             status: OrderStatusEnum::from($data['status']),
+            email: $data['email'],
+            vatRate: (int) ($data['vat_rate'] ?? 0),
+            isCompany: (bool) ($data['is_company'] ?? 0),
+            companyName: $data['company_name'] ?? null,
+            vatNumber: $data['vat_number'] ?? null,
+            totalPriceGross: (int) ($data['total_price_gross'] ?? 0),
+            shippingCostsGross: (int) ($data['shipping_costs_gross'] ?? 0),
+            invoiceAddress: AddressDto::fromArray($data['invoice_address'] ?? []),
+            shippingAddress: ! blank($data['shipping_address'])
+                ? AddressDto::fromArray($data['shipping_address'])
+                : null,
+            shippingRule: ShippingRuleDto::fromArray($data['shipping_rule']),
             createdAt: Carbon::parse($data['created_at']),
             updatedAt: Carbon::parse($data['updated_at']),
         );
