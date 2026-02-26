6e02b7ed34803871b6f0ad940e639c5f35b50951
KEIHART.online <john@keihart.online>
2026-02-26 16:45:10 +0100

    CHG: update order dto

--- src/Dto/OrderItemDto.php
+++ src/Dto/OrderItemDto.php
@@ -7,7 +7,10 @@ final readonly class OrderItemDto
     public function __construct(
         public int $quantity,
         public int $unitPriceGross,
-        public int $totalPriceGross,
+        public int $unitPriceNet,
+        public int $lineTotalGross,
+        public int $lineTotalNet,
+        public int $lineTotalVat,
         public string $articleNumber,
         public string $name,
         public bool $isChild,
@@ -19,7 +22,10 @@ final readonly class OrderItemDto
         return new self(
             quantity: (int) ($data['quantity'] ?? 0),
             unitPriceGross: (int) ($data['unit_price_gross'] ?? 0),
-            totalPriceGross: (int) ($data['total_price_gross'] ?? 0),
+            unitPriceNet: (int) ($data['unit_price_net'] ?? 0),
+            lineTotalGross: (int) ($data['line_total_gross'] ?? 0),
+            lineTotalNet: (int) ($data['line_total_net'] ?? 0),
+            lineTotalVat: (int) ($data['line_total_vat'] ?? 0),
             articleNumber: $data['article_number'] ?? null,
             name: $data['name'] ?? null,
             isChild: (bool) ($data['is_child'] ?? false),
