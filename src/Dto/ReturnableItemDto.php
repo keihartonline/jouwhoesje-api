bc052817c7a3e184e7ef8863335d9f33dd9e55b6
KEIHART.online <john@keihart.online>
2026-03-18 14:40:35 +0100

    ADD: introduced a ReturnableItemDto

---
+++ src/Dto/ReturnableItemDto.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace KeihartOnline\JouwHoesjeApi\Dto;
+
+final readonly class ReturnableItemDto
+{
+    public function __construct(
+        public int $quantity,
+        public int $unitPriceGross,
+        public int $unitPriceNet,
+        public string $articleNumber,
+        public string $name,
+        public BuyableDto $buyable,
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            quantity: (int) ($data['quantity'] ?? 0),
+            unitPriceGross: (int) ($data['unit_price_gross'] ?? 0),
+            unitPriceNet: (int) ($data['unit_price_net'] ?? 0),
+            articleNumber: $data['article_number'] ?? null,
+            name: $data['name'] ?? null,
+            buyable: BuyableDto::fromArray($data['buyable']),
+        );
+    }
+}
