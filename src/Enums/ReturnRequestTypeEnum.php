f6f43b38d1d53143bfb01df0c876098de0127d84
KEIHART.online <john@keihart.online>
2026-03-19 13:36:26 +0100

    CHG: change type enum

--- src/Enums/ReturnRequestTypeEnum.php
+++ src/Enums/ReturnRequestTypeEnum.php
@@ -4,6 +4,19 @@ namespace KeihartOnline\JouwHoesjeApi\Enums;
 
 enum ReturnRequestTypeEnum: string
 {
-    case CUSTOMER = 'customer';
-    case ADMIN = 'admin';
+    case RETURN = 'return';
+    case COULANCE = 'coulance';
+
+    public function label(): string
+    {
+        return self::getLabel($this);
+    }
+
+    public static function getLabel(self $value): string
+    {
+        return match ($value) {
+            self::RETURN => 'Retouraanvraag',
+            self::COULANCE => 'Coulanceverzoek',
+        };
+    }
 }
