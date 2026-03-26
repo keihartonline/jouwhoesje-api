991b6dc01e1f2e236fa00ff2f9fa26170de64380
KEIHART.online <john@keihart.online>
2026-03-26 06:36:46 +0100

    CHG: add follow up status array

--- src/Enums/ReturnRequestStatusEnum.php
+++ src/Enums/ReturnRequestStatusEnum.php
@@ -9,7 +9,6 @@ enum ReturnRequestStatusEnum: string
     case RECEIVED = 'received';
     case PROCESSED = 'processed';
 
-
     public function label(): string
     {
         return self::getLabel($this);
@@ -22,6 +21,36 @@ enum ReturnRequestStatusEnum: string
             self::IN_TRANSIT => 'Onderweg',
             self::RECEIVED => 'Ontvangen',
             self::PROCESSED => 'Verwerkt',
+        };
+    }
+
+    /**
+     * @return self[]
+     */
+    public function followUpStatuses(): array
+    {
+        return self::getFollowUpStatuses($this);
+    }
+
+    /**
+     * @return self[]
+     */
+    public static function getFollowUpStatuses(self $value): array
+    {
+        return match ($value) {
+            self::CREATED => [
+                self::IN_TRANSIT,
+                self::RECEIVED,
+                self::PROCESSED,
+            ],
+            self::IN_TRANSIT => [
+                self::RECEIVED,
+                self::PROCESSED,
+            ],
+            self::RECEIVED => [
+                self::PROCESSED,
+            ],
+            self::PROCESSED => [],
         };
     }
 }
