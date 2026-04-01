e3ef024a9b156977f94340487440a8afafb6b0f7
KEIHART.online <john@keihart.online>
2026-04-01 10:34:40 +0200

    CHG: add prices

--- src/Dto/ReturnRequestItemDto.php
+++ src/Dto/ReturnRequestItemDto.php
@@ -10,6 +10,9 @@ final readonly class ReturnRequestItemDto
         public ReturnReasonEnum $reason,
         public ?string $reasonNote,
         public int $quantity,
+        public int $lineTotalNet,
+        public int $lineTotalVat,
+        public int $lineTotalGross,
         public OrderItemDto $orderItem,
     ) {}
 
@@ -19,6 +22,9 @@ final readonly class ReturnRequestItemDto
             reason: ReturnReasonEnum::from($data['reason']),
             reasonNote: $data['reason_note'] ?? null,
             quantity: $data['quantity'],
+            lineTotalNet: (int) ($data['line_total_net'] ?? 0),
+            lineTotalVat: (int) ($data['line_total_vat'] ?? 0),
+            lineTotalGross: (int) ($data['line_total_gross'] ?? 0),
             orderItem: OrderItemDto::fromArray($data['order_item']),
         );
     }
