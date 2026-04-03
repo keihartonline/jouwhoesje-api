053d34d5626e872f7d124669cf7a501deaf2fbe1
KEIHART.online <john@keihart.online>
2026-04-03 10:01:40 +0200

    CHG: add cutoff time

--- src/Dto/ShopDto.php
+++ src/Dto/ShopDto.php
@@ -19,6 +19,7 @@ final readonly class ShopDto
         public string $dialCode,
         public int $returnDays,
         public int $returnPrice,
+        public string $cutoffTime,
         public CountryDto $country,
         public array $paymentMethods = [],
         public array $countries = [],
@@ -37,6 +38,7 @@ final readonly class ShopDto
             dialCode: $data['dial_code'],
             returnDays: $data['return_days'],
             returnPrice: $data['return_price'],
+            cutoffTime: $data['cutoff_time'],
             country: CountryDto::fromArray($data['country']),
             paymentMethods: array_map(
                 fn (array $paymentMethod) => PaymentMethodDto::fromArray($paymentMethod),
