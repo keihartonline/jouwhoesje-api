3a317c758f926fbbcef948b35af8ad7a46de81db
KEIHART.online <john@keihart.online>
2026-04-03 11:53:15 +0200

    CHG: add label to types

--- src/Enums/DeviceTypeEnum.php
+++ src/Enums/DeviceTypeEnum.php
@@ -7,4 +7,18 @@ enum DeviceTypeEnum: string
     case IPAD = 'ipad';
     case MACBOOK = 'macbook';
     case PHONE = 'phone';
+
+    public function label(): string
+    {
+        return self::getLabel($this);
+    }
+
+    public static function getLabel(self $value): string
+    {
+        return match ($value) {
+            self::IPAD => 'iPad',
+            self::MACBOOK => 'MacBook',
+            self::PHONE => 'Telefoon',
+        };
+    }
 }
