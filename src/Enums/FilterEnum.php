1c261ce4e8e00836ffda6d859bdbd7a418be96c9
KEIHART.online <john@keihart.online>
2026-06-16 12:02:36 +0200

    Revert "CHG: add device as a filter"
    This reverts commit 8ecd7df16a307223f8f14c4c0bf14ecfe858869e.

--- src/Enums/FilterEnum.php
+++ src/Enums/FilterEnum.php
@@ -5,7 +5,6 @@ namespace KeihartOnline\JouwHoesjeApi\Enums;
 enum FilterEnum: string
 {
     case COVER_DEVICE_LINK = 'cover_device_link';
-    case DEVICE = 'device';
     case ACCESSORY_BRAND = 'accessory_brand';
     case DATASHEET = 'datasheet';
     case CATEGORY = 'category';
