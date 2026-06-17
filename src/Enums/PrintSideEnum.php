95d159040b792e500f730247a6bb076139344ade
KEIHART.online <john@keihart.online>
2026-06-17 16:37:03 +0200

    CHG: add label to print side enum

--- src/Enums/PrintSideEnum.php
+++ src/Enums/PrintSideEnum.php
@@ -7,4 +7,25 @@ enum PrintSideEnum: string
     case BACK_PRINTED = 'back-printed';
     case FRONT_PRINTED = 'front-printed';
     case FULLY_PRINTED = 'fully-printed';
+
+    public function label(): string
+    {
+        return self::getLabel($this);
+    }
+
+    public static function getLabel(self $value): string
+    {
+        return match ($value) {
+            self::BACK_PRINTED => 'Achterkant bedrukt',
+            self::FRONT_PRINTED => 'Voorkant bedrukt',
+            self::FULLY_PRINTED => 'Volledig bedrukt',
+        };
+    }
+
+    public static function options(): array
+    {
+        return collect(self::cases())->mapWithKeys(
+            fn ($item) => [$item->value => self::getLabel($item)]
+        )->toArray();
+    }
 }
